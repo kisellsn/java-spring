61d4b5c23d964a321d47dc3a154fea87550b79ee
Yaroslav Hordiienko <gordienkoyar1@gmail.com>
2023-12-09 20:36:49 +0200

    Implemented QueueService interface

--- lab2/src/main/java/com/example/demo/service/QueueService.java
+++ lab2/src/main/java/com/example/demo/service/QueueService.java
@@ -1,73 +1,28 @@
 package com.example.demo.service;
+
 import com.example.demo.model.Queue;
 import com.example.demo.model.QueueEntry;
-import com.example.demo.repositories.QueueRepositoryFake;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class QueueService {
+public interface QueueService {
 
-    private final QueueRepositoryFake queueRepository;
+    Queue createQueue(String name, String ownerName, String password);
 
-    @Autowired
-    public QueueService(QueueRepositoryFake queueRepository) {
-        this.queueRepository = queueRepository;
-    }
-//    @Autowired
-//    public void setQueueRepository(QueueRepositoryStub queueRepository) {
-//        this.queueRepository = queueRepository;
-//    }
+    void joinQueue(Queue queue, String userName);
 
-    public Queue createQueue(String name, String ownerName, String password) {
-        Queue queue = new Queue(name, ownerName, password);
-        return queueRepository.saveQueue(queue);
-    }
+    List<Queue> getAllQueues();
 
-    public void joinQueue(Queue queue, String userName) {
-        QueueEntry entry = new QueueEntry(queue, userName);
-        queue.addEntry(entry);
-    }
+    List<QueueEntry> getQueueEntriesByQueue(Queue queue);
 
-    public List<Queue> getAllQueues() {
-        return queueRepository.getAllQueues();
-    }
+    void removeQueueEntry(Queue queue, String userName);
 
-    public List<QueueEntry> getQueueEntriesByQueue(Queue queue) {
-        return queue.getQueueEntries();
-    }
+    void removeNextEntry(Queue queue);
 
-    public void removeQueueEntry(Queue queue, String userName) {
-        queue.getQueueEntries().removeIf(entry -> entry.getUserName().equals(userName));
+    void closeQueue(Queue queue);
 
-    }
-    public void removeNextEntry(Queue queue) {
-        queue.removeEntry();
-    }
+    Optional<Queue> getQueueByName(String name);
 
-    public void closeQueue(Queue queue) {
-        queueRepository.closeQueue(queue);
-    }
-
-
-    public Optional<Queue> getQueueByName(String name) {
-        return queueRepository.getQueueByName(name);
-    }
-
-    public List<QueueEntry> getUserEntries(String userName) {
-        List<Queue> queues = queueRepository.getAllQueues();
-        List<QueueEntry> entries = new ArrayList<>();
-        for (Queue queue : queues) {
-            for (QueueEntry entry : queue.getQueueEntries()) {
-                if (entry.getUserName().equals(userName)) {
-                    entries.add(entry);
-                }
-            }
-        }
-        return entries;
-    }
+    List<QueueEntry> getUserEntries(String userName);
 }
