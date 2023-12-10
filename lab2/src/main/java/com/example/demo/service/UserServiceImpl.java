a307f0a9c8aedeab871a51705d30584da018b86a
Yaroslav Hordiienko <gordienkoyar1@gmail.com>
2023-12-10 11:16:37 +0200

    Refactor: added Service annotation for UserServiceImpl

--- lab2/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ lab2/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.demo.service;
 import com.example.demo.model.User;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
